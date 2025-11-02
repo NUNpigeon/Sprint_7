a39be8214f66855726bca8c4b7b385c47b9cd768
Natalia Beskurnikova <bskurnikova@yandex.ru>
2025-11-02 15:49:36 +0300

    Modified 02.11.25

--- src/test/java/OrderApi.java
+++ src/test/java/OrderApi.java
@@ -1,4 +1,5 @@
 import io.restassured.response.ValidatableResponse;
+import io.qameta.allure.Step;
 
 import static io.restassured.RestAssured.given;
 import static org.apache.http.HttpStatus.*;
@@ -7,33 +8,35 @@ public class OrderApi {
 
     private static final String ORDERS_ENDPOINT = "/api/v1/orders";
 
-    // Получение списка заказов
+    @Step("Получение списка заказов")
     public ValidatableResponse getOrders() {
+        // Отправляю GET-запрос для получения списка заказов
         return given()
-                .header("Content-type", "application/json")
+                .header("Content-type", "application/json") // Устанавливаю заголовок Content-Type
                 .when()
-                .get(ORDERS_ENDPOINT)
-                .then().log().all();
+                .get(ORDERS_ENDPOINT) // Выполняю GET-запрос к эндпоинту
+                .then().log().all(); // Логирую все детали ответа в консоль для отладки
     }
 
-    // Создание заказа
+    @Step("Создание заказа")
     public ValidatableResponse createOrder(Object order) {
+        // Отправляю POST-запрос для создания заказа
         return given()
-                .header("Content-type", "application/json")
-                .body(order)
+                .header("Content-type", "application/json") // Устанавливаю заголовок Content-Type
+                .body(order) // Устанавливаю тело запроса (данные заказа)
                 .when()
-                .post(ORDERS_ENDPOINT)
-                .then();
+                .post(ORDERS_ENDPOINT) // Выполняю POST-запрос к эндпоинту
+                .then(); // Возвращаю объект ValidatableResponse для дальнейших проверок
     }
 
-    // Отмена заказа
+    @Step("Отмена заказа с трек-номером: {track}")
     public void cancelOrder(String track) {
+        // Отправляю PUT-запрос для отмены заказа
         given()
-                .header("Content-type", "application/json")
+                .header("Content-type", "application/json") // Устанавливаю заголовок Content-Type
                 .when()
-                .put("/api/v1/orders/cancel?track=" + track)
+                .put("/api/v1/orders/cancel?track=" + track) // Выполняю PUT-запрос к эндпоинту отмены заказа
                 .then()
-                .statusCode(SC_OK);  // Проверяю, что ответ имеет статус 200 (OK) при отмене
+                .statusCode(SC_OK);  // Првоеряю, что статус код ответа равен 200
     }
 }
-
