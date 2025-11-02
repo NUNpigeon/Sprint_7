2e3c3ec186b4e87affbb54f78cbe3c9ba6ea02fa
Natalia Beskurnikova <bskurnikova@yandex.ru>
2025-11-02 15:51:46 +0300

    Modified 02.11.25_2

--- src/test/java/OrderListTest.java
+++ src/test/java/OrderListTest.java
@@ -1,7 +1,6 @@
 import io.restassured.response.ValidatableResponse;
 import org.junit.Test;
 import io.qameta.allure.junit4.DisplayName;
-import io.qameta.allure.Step;
 
 import static org.hamcrest.Matchers.*;
 
@@ -14,24 +13,14 @@ public class OrderListTest extends ApiTest {
     @Test
     @DisplayName("Запрос списка заказов возвращает заказы")
     public void getOrderListReturnsOrders() {
-        // Получаем ответ на запрос списка заказов, используя метод с аннотацией @Step
-        ValidatableResponse response = getOrders();
+        ValidatableResponse response = orderApi.getOrders();
 
-        // Проверяем, что вернулся код 200 (OK)
-        response.assertThat().statusCode(SC_OK);
+        response.assertThat().statusCode(SC_OK); // Проверяю, что вернулся код 200 (OK)
 
-        // Проверяем, что тело ответа не пустое
         response.assertThat().body(notNullValue());
 
-        // Проверяем, что "orders" - это список (List)
         response.assertThat().body("orders", instanceOf(List.class));
 
-        // Проверяем, что размер списка "orders" больше или равен 0
         response.assertThat().body("orders.size()", greaterThanOrEqualTo(0));
-    }
-
-    @Step("Получение списка заказов")
-    private ValidatableResponse getOrders() {
-        return orderApi.getOrders(); // Вызов метода API для получения списка заказов
     }
 }
